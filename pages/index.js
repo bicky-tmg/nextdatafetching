c7cf4f3e88de3943b064c8d0c786a068fd24e056
Bicky Tamang <bickytamang101@gmail.com>
2022-05-25 12:02:23 +0545

    getStaticProps and configuration options

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,19 @@ export async function getStaticProps() {
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data",
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
+
   return {
     props: {
       products: data.products,
