9e39356d1aee6c07309181ddb1e783ae0fe3e997
Bicky Tamang <bickytamang101@gmail.com>
2022-05-25 14:04:43 +0545

    dynamic pages & getServerSideProps

--- pages/user-profile.js
+++ pages/user-profile.js
@@ -5,6 +5,8 @@ function UserProfilePage(props) {
 export default UserProfilePage;
 
 export async function getServerSideProps(context) {
+  const { params, req, res } = context;
+
   return {
     props: {
       userName: "Bicky",
