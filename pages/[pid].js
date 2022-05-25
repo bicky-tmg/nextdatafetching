6a8230c9f34bca7eb841944fe9d2f88df336a062
Bicky Tamang <bickytamang101@gmail.com>
2022-05-25 12:15:19 +0545

    working with dynamic parameters

---
+++ pages/[pid].js
@@ -0,0 +1,32 @@
+import path from "path";
+import fs from "fs/promises";
+
+function ProductDetailPage(props) {
+  const { loadedProduct } = props;
+  return (
+    <>
+      <h1>{loadedProduct.title}</h1>
+      <p>{loadedProduct.description}</p>
+    </>
+  );
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+
+  const productId = params.pid;
+
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  const product = data.products.find((product) => product.id === productId);
+
+  return {
+    props: {
+      loadedProduct: product,
+    },
+  };
+}
+
+export default ProductDetailPage;
