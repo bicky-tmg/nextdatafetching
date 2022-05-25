733ff593e6331e55ebbf28766ab8a9aaf4da2cfb
Bicky Tamang <bickytamang101@gmail.com>
2022-05-25 11:51:49 +0545

    Utilizing ISR

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,7 @@ function HomePage(props) {
 }
 
 export async function getStaticProps() {
+  console.log("Re-Generating...");
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
@@ -20,6 +21,7 @@ export async function getStaticProps() {
     props: {
       products: data.products,
     },
+    revalidate: 10,
   };
 }
 
